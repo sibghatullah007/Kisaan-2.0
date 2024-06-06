af942e67e7169e4e7f30c1b10e2942a4e3b1df02
asadREHMAN <asad@gmail.com>
2024-06-06 21:06:28 +0500

    💪 Model integration is now done correctly and working perfectly

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
     }
     buildFeatures {
         compose = true
+        mlModelBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -64,6 +65,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     implementation("androidx.compose.runtime:runtime-livedata:1.6.7")
+    implementation("org.tensorflow:tensorflow-lite-metadata:0.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/final_year_project/kisaan10/ViewModel/WheatViewModel.kt
+++ app/src/main/java/com/final_year_project/kisaan10/ViewModel/WheatViewModel.kt
@@ -9,11 +9,15 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.final_year_project.kisaan10.ml.Detectmodel
+import com.final_year_project.kisaan10.ml.Wheatmodel
 import com.final_year_project.kisaan10.model.DiseasePredictionResult
 import com.final_year_project.kisaan10.model.WheatDetectionResult
 import kotlinx.coroutines.launch
+import org.tensorflow.lite.DataType
 import java.io.IOException
 import org.tensorflow.lite.Interpreter
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
 import java.io.FileInputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
@@ -24,12 +28,17 @@ import java.nio.channels.FileChannel
 const val TAG = "WheatViewModel"
 class WheatViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val detectModelInterpreter: Interpreter
-    private val wheatModelInterpreter: Interpreter
+//    private val detectModelInterpreter: Interpreter
+//    private val wheatModelInterpreter: Interpreter
+    private val wheatModel: Wheatmodel
+    private val detectModel: Detectmodel
 
     init {
-        detectModelInterpreter = loadModel(application, "Detectmodel.tflite")
-        wheatModelInterpreter = loadModel(application, "Wheatmodel.tflite")
+//        detectModelInterpreter = loadModel(application, "Detectmodel.tflite")
+//        wheatModelInterpreter = loadModel(application, "Wheatmodel.tflite")
+        wheatModel = Wheatmodel.newInstance(application)
+        detectModel = Detectmodel.newInstance(application)
+
     }
 
     private fun loadModel(context: Context, modelName: String): Interpreter {
@@ -80,26 +89,21 @@ class WheatViewModel(application: Application) : AndroidViewModel(application) {
 
     private fun runDetectWheatModel(image: Bitmap): WheatDetectionResult {
         val bitmap = Bitmap.createScaledBitmap(image, 224, 224, true)
-        val input = ByteBuffer.allocateDirect(224 * 224 * 3 * 4).order(ByteOrder.nativeOrder())
-        for (y in 0 until 224) {
-            for (x in 0 until 224) {
-                val px = bitmap.getPixel(x, y)
-                val r = Color.red(px)
-                val g = Color.green(px)
-                val b = Color.blue(px)
-                input.putFloat((r - 127) / 255f)
-                input.putFloat((g - 127) / 255f)
-                input.putFloat((b - 127) / 255f)
-            }
-        }
 
-        val bufferSize = 2 * java.lang.Float.SIZE / java.lang.Byte.SIZE
-        val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
-        detectModelInterpreter.run(input, modelOutput)
+        val byteBuffer = convertBitmapToByteBuffer(bitmap)
 
-        modelOutput.rewind()
-        val probabilities = modelOutput.asFloatBuffer()
-        val isWheat = probabilities.get(0) > probabilities.get(1)
+        // Creates inputs for reference
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
+
+        // Runs model inference and gets result
+        val outputs = detectModel.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        // Process the output
+        val probabilities = outputFeature0.floatArray
+        val isWheat = probabilities[0] > probabilities[1]
+
         return WheatDetectionResult(isWheat)
     }
 
@@ -111,41 +115,48 @@ class WheatViewModel(application: Application) : AndroidViewModel(application) {
         }
 
         val bitmap = Bitmap.createScaledBitmap(image, 224, 224, true)
-        val input = ByteBuffer.allocateDirect(224 * 224 * 3 * 4).order(ByteOrder.nativeOrder())
-        for (y in 0 until 224) {
-            for (x in 0 until 224) {
-                val px = bitmap.getPixel(x, y)
-                val r = Color.red(px)
-                val g = Color.green(px)
-                val b = Color.blue(px)
-                input.putFloat((r - 127) / 255f)
-                input.putFloat((g - 127) / 255f)
-                input.putFloat((b - 127) / 255f)
-            }
-        }
 
-        val bufferSize = 5 * java.lang.Float.SIZE / java.lang.Byte.SIZE
-        val modelOutput = ByteBuffer.allocateDirect(bufferSize).order(ByteOrder.nativeOrder())
-        wheatModelInterpreter.run(input, modelOutput)
+        val byteBuffer = convertBitmapToByteBuffer(bitmap)
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(byteBuffer)
 
-        modelOutput.rewind()
-        val probabilities = FloatArray(5)
-        modelOutput.asFloatBuffer().get(probabilities)
+        val outputs = wheatModel.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
 
-        // Find the index of the maximum probability
+        val probabilities = outputFeature0.floatArray
         val maxIndex = probabilities.indices.maxByOrNull { probabilities[it] } ?: -1
-
-        // Map the index to the disease name
-        val diseaseNames = listOf("Septoria Leaf Blotch", "Loose Smut", "Healthy Wheat", "Brown Rust (Leaf Rust)","Yellow Rust (Stripe Rust)")
+        val diseaseNames = listOf("Septoria Leaf Blotch", "Loose Smut", "Healthy Wheat", "Brown Rust (Leaf Rust)", "Yellow Rust (Stripe Rust)")
         val diseaseName = if (maxIndex != -1) diseaseNames[maxIndex] else "Unknown"
-
-        // Get the confidence score for the predicted disease
         val confidence = if (maxIndex != -1) probabilities[maxIndex] else 0.0f
 
+
+        Log.v(TAG,diseaseName + confidence)
         return DiseasePredictionResult(diseaseName, confidence)
     }
 
+    fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val inputSize = 224
+        val byteBuffer = ByteBuffer.allocateDirect(4 * inputSize * inputSize * 3) // 4 bytes per float, 3 channels
+        byteBuffer.order(ByteOrder.nativeOrder())
 
+        val scaledBitmap = Bitmap.createScaledBitmap(bitmap, inputSize, inputSize, true)
+
+        val intValues = IntArray(inputSize * inputSize)
+        scaledBitmap.getPixels(intValues, 0, scaledBitmap.width, 0, 0, scaledBitmap.width, scaledBitmap.height)
+
+        var pixel = 0
+        for (i in 0 until inputSize) {
+            for (j in 0 until inputSize) {
+                val pixelValue = intValues[pixel++]
+
+                // Normalize pixel value to [0, 1] and add to ByteBuffer
+                byteBuffer.putFloat((Color.red(pixelValue) - 127.5f) / 127.5f)
+                byteBuffer.putFloat((Color.green(pixelValue) - 127.5f) / 127.5f)
+                byteBuffer.putFloat((Color.blue(pixelValue) - 127.5f) / 127.5f)
+            }
+        }
+        return byteBuffer
+    }
 
 
 
